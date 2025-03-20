1aa818fcaae1c2cf75df75feefa1155aba5c5b50
heygus09 <carlos.ramirez6@catolica.edu.sv>
2025-03-19 21:07:21 -0600

    Modelo economicos
    Se creo el modelo economicos y se lleno

--- app/Models/economico.php
+++ app/Models/economico.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class economico extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'economicos';
+    protected $primaryKey = 'id';
+    protected $perPage = 20;
+
+    protected $fillable = 
+    [
+        'yearr',
+        'mes',
+        'numcitas',
+        'ingresos'
+    ];
+
+    public $timestamps = true;
 }
