295c2b35acee98b548f10220f5cc2567cbf67412
heygus09 <carlos.ramirez6@catolica.edu.sv>
2025-03-21 12:39:35 -0600

    Update 2025_03_16_040802_create_expedientes_table.php
    se creo el schema de la migration de la tabla expedientes

--- database/migrations/2025_03_16_040802_create_expedientes_table.php
+++ database/migrations/2025_03_16_040802_create_expedientes_table.php
@@ -12,9 +12,31 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('expedientes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('numeroexpediente'); // Clave primaria
+            $table->unsignedBigInteger('idusuario'); // Clave foránea
+            $table->date('fechacreacion');
+            $table->integer('numcitas')->nullable();
+            $table->longText('diagnostico')->nullable();
+            $table->date('fechaevaluacion')->nullable();
+            $table->longText('historiaclinica')->nullable();
+            $table->mediumText('observacion')->nullable();
+            $table->mediumText('palpacion')->nullable();
+            $table->mediumText('sensibilidad')->nullable();
+            $table->mediumText('arcosdemovimiento')->nullable();
+            $table->mediumText('fuerzamuscular')->nullable();
+            $table->mediumText('perimetria')->nullable();
+            $table->mediumText('longitudmiembrosinf')->nullable();
+            $table->mediumText('marcha')->nullable();
+            $table->mediumText('postura')->nullable();
+            $table->string('nombrefisioterapeuta', 50)->default('Licda. Xiomara Meza');
+            $table->longText('notasevolutivas')->nullable();
+            $table->enum('estado', ['Abierto', 'Cerrado'])->nullable();
+
+            
+            // Definir clave foránea
+            $table->foreign('idusuario')->references('idusuario')->on('usuarios')->onDelete('cascade');
         });
+        
     }
 
     /**
