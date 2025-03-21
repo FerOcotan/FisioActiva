d8778ef7f3dc3c1d190c1dfdfbc235059e4335b5
heygus09 <carlos.ramirez6@catolica.edu.sv>
2025-03-20 21:46:53 -0600

    Modelo usuarios
    Se creo el modelo usuarios y se lleno

--- app/Models/usuarios.php
+++ app/Models/usuarios.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class usuarios extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id';
+    protected $perPage = 20;
+
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'edad',
+        'genero',
+        'direccion',
+        'latitud',
+        'longitud',
+        'telefono',
+        'correo',
+        'contrasena',
+        'rol',
+        'estado',
+    ];
+
+    public $timestamps = true;
+    protected $hiden = [
+        'contrasena',
+    ];
+
+    protected $casts = [
+        'contrsena' => 'hashed',
+    ];
 }
