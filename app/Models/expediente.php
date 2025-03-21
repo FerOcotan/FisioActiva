e0714b8486ce3eb6e7070303920bb41f1e6c8d45
heygus09 <carlos.ramirez6@catolica.edu.sv>
2025-03-21 12:40:37 -0600

    Modelo expedientes
    Se creo el modelo expedientes y se lleno

--- app/Models/expediente.php
+++ app/Models/expediente.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class expediente extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'expedientes';
+    protected $primaryKey = 'numeroexpediente';
+    protected $perPage = 20;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'idusuario',
+        'fechacreacion',
+        'numcitas',
+        'diagnostico',
+        'fechaevaluacion',
+        'historiaclinica',
+        'observacion',
+        'palpacion',
+        'sensibilidad',
+        'arcosdemovimiento',
+        'fuerzamuscular',
+        'perimetria',
+        'longitudmiembrosinf',
+        'marcha',
+        'postura',  
+        'nombrefisioterapeuta',
+        'notasevolutivas',
+        'estado',
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(usuarios::class, 'idusuario', 'idusuario');
+    }
 }
