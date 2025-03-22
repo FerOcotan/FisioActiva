1c12d6904f592037a7a18ba8e5c23a858b2d2fc2
heygus09 <carlos.ramirez6@catolica.edu.sv>
2025-03-22 00:25:08 -0600

    Modelo citas
    Se creo el modelo citas, se lleno y se relaciono con las tablas expedientes y usuarios

--- app/Models/cita.php
+++ app/Models/cita.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class cita extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'citas';
+    protected $primaryKey = 'numerocita';
+    public $timestamps = true;
+    protected $perPage = 20;
+
+    protected $fillable = 
+    [
+        'numeroexpediente',  
+        'fechahora',
+        'modalidad', 
+        'cargo',
+        'estado',
+    ];
+
+    public function expediente()
+    {
+        return $this->belongsTo(expediente::class, 'numeroexpediente', 'numeroexpediente');
+    }
+
+    public function usuario()
+{
+    return $this->belongsTo(usuarios::class, 'idusuario'); 
+}
+
 }
