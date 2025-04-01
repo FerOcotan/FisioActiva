0e37e5395655c57fd658baa7c6ecbffc36657f35
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-01 09:21:43 -0600

    add style login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,9 +12,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <!-- Se agrega el fondo en el body -->
-  <body class="font-sans text-gray-900 antialiased">
+  <body class="font-sans text-gray-900 antialiased" style="background-image: url('{{ asset('build/assets/images/fondodvg.svg') }}'); background-size: cover; background-position: center; background-attachment: fixed;">
     <!-- Contenedor principal para centrar todo y ocupar toda la pantalla -->
-    <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="flex justify-center items-center min-h-screen bg-black bg-opacity-50"> <!-- Añadido bg-opacity para que se vea el fondo -->
         <!-- Contenedor de dos columnas (formulario y imagen) -->
         <div class="w-full sm:w-10/12 md:w-8/12 lg:w-1/2 flex rounded-lg shadow-lg overflow-hidden">
             <!-- Columna del formulario con fondo -->
@@ -30,8 +30,6 @@
                     <div class="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg text-black">
                         {{ $slot }}
                     </div>
-
-                    
                 </div>
             </div>
 
