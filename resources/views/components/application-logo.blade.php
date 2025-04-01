1a0db6a792a00a1bc0342402002e6bebae6008be
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-31 22:15:44 -0600

    estilos dash

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,4 @@
 <script src="https://cdn.tailwindcss.com"></script>
-<h1 class="text-4xl  text-white-100 text-center font-bold">Fisio<span class="font-normal text-white">Activa</span></h1>
+<h1 class="text-4xl  
+text-white-100 text-center font-bold">Fisio<span 
+class="font-normal text-white">Activa</span></h1>
---
+++ resources/views/components/application-logo2.blade.php
@@ -0,0 +1,4 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<h1 class="text-4xl  
+text-blue-900 text-center font-bold">Fisio<span 
+class="font-bold text-white">Activa</span></h1>
