6fc282843db55977399b3d4d6b0d21e5a15f41bb
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-05-06 22:44:58 -0600

    try4

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
+
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+
+        
     ],
     build: {
-        outDir: 'public/build',
+        // Copiar imágenes de resources a public
         assetsDir: 'images',
         manifest: true,
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                entryFileNames: 'images/[name]-[hash].js',
-                chunkFileNames: 'images/[name]-[hash].js',
-                assetFileNames: ({ name }) => {
-                    if (name && name.endsWith('.css')) {
-                        return 'images/[name]-[hash][extname]';
-                    }
-                    return 'images/[name]-[hash][extname]';
-                },
-            },
-        }
     }
 });
