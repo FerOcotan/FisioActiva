de500c5fd02cc27f2b17a8bff2db9b626c43119f
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-05-06 22:07:31 -0600

    1try to deploy

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -96,6 +96,7 @@ class="relative flex h-screen bg-white"
     @csrf
     <button type="submit" class="flex items-center justify-center w-full p-3 rounded-lg transition-all duration-200 group">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
       </svg>
       <span class="ml-3 whitespace-nowrap group-hover:text-red-600">Cerrar Sesión</span>
     </button>
