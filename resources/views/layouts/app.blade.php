39302ce2709ceb45efc771d18c418e883468312a
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-05-06 22:27:11 -0600

    2try2  to deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,33 +3,25 @@
     <head>
         <style>
             [x-cloak] { display: none !important; }
-          </style>
+        </style>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
         @vite('resources/css/app.css')
         @vite(['resources/js/app.js'])
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         <title>{{ config('app.name', 'FisioActiva') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-blue-100">
             @include('layouts.navigation')
             @include('sidebar')
-
         </div>
     </body>
 </html>
-
-
-@stack('scripts')
